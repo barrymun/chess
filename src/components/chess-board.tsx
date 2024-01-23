c1ef8fff1c0ff65056c3d2592fe0ab1f1a199a96
Neil Murphy <barrymun@tcd.ie>
2024-01-23 22:24:44 +0000

    setting board and tile and tile colours, setting tw colour theme defaults

--- src/components/chess-board.tsx
+++ src/components/chess-board.tsx
@@ -3,21 +3,14 @@ const yArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const ChessBoard = () => {
   return (
-    <div className="flex content-center items-center flex-col">
-      <div className="border-2">
+    <div className="flex content-center items-center flex-col justify-center">
+      <div className="bg-chess-board border-2 rounded-sm">
         {yArray.map((y) => {
           return (
             <div key={y} className="flex">
               {xArray.map((x) => {
-                return (
-                  <div
-                    key={x}
-                    className="w-24 h-24 flex"
-                    style={{
-                      backgroundColor: (x.charCodeAt(0) + y) % 2 === 0 ? "black" : "white",
-                    }}
-                  />
-                );
+                const isEven = (x.charCodeAt(0) + y) % 2 === 0;
+                return <div key={x} className={`w-24 h-24 flex ${isEven ? "bg-chess-tile" : "bg-inherit"}`} />;
               })}
             </div>
           );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'chess-board': '#769656',
+        'chess-tile': '#eeeed2',
+      },
+    },
   },
   plugins: [],
 }
