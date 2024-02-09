cbfb8325a5f029346535bd2c1613f51ef2775ed5
Neil Murphy <barrymun@tcd.ie>
2024-02-09 13:10:00 +0000

    fix page contents appearing behind header on scroll

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,6 +1,8 @@
+import { grayDark } from "@radix-ui/colors";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { Box, IconButton } from "@radix-ui/themes";
-import { FC } from "react";
+import { Box, IconButton, Text } from "@radix-ui/themes";
+import { FC, useMemo } from "react";
+import { Link } from "react-router-dom";
 
 import { useTheme } from "hooks";
 
@@ -11,23 +13,29 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = ({ children }) => {
   const { appearance, setAppearance } = useTheme();
 
+  const bgColor = useMemo(() => (appearance === "dark" ? `bg-[${grayDark.gray1}]` : "bg-[#ffffff]"), [appearance]);
+
   const handleClick = () => {
     setAppearance(appearance === "dark" ? "light" : "dark");
   };
 
   return (
     <>
-      <header className="bg-gray-800 p-4">
-        <Box className="flex items-center justify-between">
-          <Box>
-            <h1 className="text-3xl text-white">Chess</h1>
+      <Box className={`fixed top-0 z-10 w-full shadow-lg ${bgColor}`}>
+        <header className="sm:p-4 xs:p-2">
+          <Box className="flex items-center justify-between">
+            <Box>
+              <Text className="text-3xl">
+                <Link to="/">Chess</Link>
+              </Text>
+            </Box>
+            <Box>
+              <IconButton onClick={handleClick}>{appearance === "dark" ? <MoonIcon /> : <SunIcon />}</IconButton>
+            </Box>
           </Box>
-          <Box>
-            <IconButton onClick={handleClick}>{appearance === "dark" ? <MoonIcon /> : <SunIcon />}</IconButton>
-          </Box>
-        </Box>
-      </header>
-      {children}
+        </header>
+      </Box>
+      <Box className="w-full h-full pt-16">{children}</Box>
     </>
   );
 };
