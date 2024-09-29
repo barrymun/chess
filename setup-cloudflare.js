d0dd958c2def8cff1bbeb3d05a447efa73bb46d0
Neil Murphy <barrymun@tcd.ie>
2024-09-29 14:32:18 +0100

    override install command

---
+++ setup-cloudflare.js
@@ -0,0 +1,16 @@
+const { execSync } = require('child_process');
+
+const opts = { stdio: 'inherit' };
+
+console.log('Cleaning common/build...');
+execSync('rm -rf common/build', opts);
+execSync('mkdir -p common/build', opts);
+
+console.log('Installing dependencies...');
+execSync('yarn install', opts);
+
+console.log('Building common package...');
+execSync('yarn workspace common build', opts);
+
+console.log('re-installing common package...');
+execSync('yarn install --check-files', opts);
